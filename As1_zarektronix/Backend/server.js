0f25bc3e9a42f8a88370bf177499e55610f30ba1
kuntalkumar <kuntalkumar789@gmail.com>
2024-03-05 00:25:46 +0530

    Backend done

---
+++ As1_zarektronix/Backend/server.js
@@ -0,0 +1,49 @@
+// server.js (Node.js backend)
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://kuntalkumar789:kuntal98@cluster0.vigwezr.mongodb.net/zarektronix', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+// decleare the schema of user
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+
+// Route
+app.get("/",(req,res)=>{
+    res.send("Api is working")
+})
+
+
+app.post('/signup', async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const newUser = await User.create({ name, email, password });
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+const PORT =8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
