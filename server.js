f08922ebc3f34ead14ba8ad88788d8139fb1da85
kuntalkumar <kuntalkumar789@gmail.com>
2024-03-05 19:52:19 +0530

    added

--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 // connected to the data base (MongoDB)
