c07421755218a54b8343c5af923e6c61b5a36313
kuntalkumar <kuntalkumar789@gmail.com>
2024-08-22 11:00:27 +0530

    Backend code working, testing done

--- equipnine/be/routes/userRoutes.js
+++ equipnine/be/routes/userRoutes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/me', authenticateToken, getUserData); // Add this line
+router.get('/me', authenticateToken, getUserData);
 
 module.exports = router;
