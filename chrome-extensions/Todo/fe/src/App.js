373ed7ce319e4b05757557eee771633fc95ca6bd
kuntalkumar <kuntalkumar789@gmail.com>
2024-08-28 15:44:52 +0530

    added

--- chrome-extensions/Todo/fe/src/App.js
+++ chrome-extensions/Todo/fe/src/App.js
@@ -5,25 +5,8 @@ function App() {
   return (
     <div className="App">
    <Todo/>
-<hr />
 
-   <table >
-    <th>SL No</th>
-    <th>Task</th>
-    <th>Status</th>
-    <th>Edit</th>
-    <th>Delete</th>
-
-
-          <tr>
-          <td>1</td>
-          <td> New tasks</td>
-          <td> Pending</td>
-          <td> Edit</td>
-          <td>Delete</td>
-          </tr>
-
-   </table>
+ 
     </div>
   );
 }
--- chrome-extensions/Todo/fe/src/Component/Todo.jsx
+++ chrome-extensions/Todo/fe/src/Component/Todo.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import "./TodoStyle.css"
 const Todo = () => {
 
+    const [data,setdata]=useState([])
+    const fetchData=async()=>{
+        let res= await fetch ("http://localhost:8080/?_page=1&_limit=10")
+        let ApiData=await res.json();
+        setdata(ApiData)
+        console.log(data)
+    }
+    useEffect(()=>{
+    fetchData()
+
+    }, [])
 
   return (
     <div>
     <h1>Task Manager</h1>
-      
+    {/* <hr /> */}
+
+
+    <ul >
+    <div>SL No</div>
+    <div>Task</div>
+    <div>Status</div>
+    <div>Delete</div>
+    </ul>
+    
+
+     {data?.map((ele , i)=>(
+        <div className='tasksList'>
+    <div>{i+1}</div>
+    <div>{ele.task}</div>
+    <div>{ele.status}</div>
+    <div>Delete</div>
+
+    </div>
+     ))} 
+     <div>Add Tasks</div>
+ 
+        
     </div>
   )
 }
