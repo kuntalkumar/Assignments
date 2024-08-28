8016ea6f71ca1735dabe3fe856705bdd65a88d9c
kuntalkumar <kuntalkumar789@gmail.com>
2024-08-28 15:44:30 +0530

    added

--- chrome-extensions/Todo/be/index.js
+++ chrome-extensions/Todo/be/index.js
@@ -1,9 +1,11 @@
 const express=require("express")
 const mongoose=require("mongoose")
 const dotenv=require("dotenv")
+const cors=require("cors")
 const app=express();
 
 app.use(express.json())
+app.use(cors())
 dotenv.config(); 
 const mongoDbUrl=process.env.MONGODB_URL
 
