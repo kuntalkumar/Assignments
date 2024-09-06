bb6c159d5b165eec3d4709b099a9fd2d7d18fda8
kuntalkumar <kuntalkumar789@gmail.com>
2024-09-06 17:40:44 +0530

    added

--- wisdom-peak-analytics/fe/src/App.js
+++ wisdom-peak-analytics/fe/src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className='top_section'>
+      <div className='topSection'>
 
-      <h2>Name : Kuntal Kumar</h2>
+          <h1> Kuntal Kumar</h1>
+          <h4>Full Web Stack Developer</h4>
+        <p>Bangalore , Karnataka </p>
+      </div>
+      <div className='profilePic'>
+        <img src={require('./asset/profile-pic.png')} alt="image" width={"200px"} />
 
       </div>
     </div>
