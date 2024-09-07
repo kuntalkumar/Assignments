7c2e6759cee1fc6f90d3aa7b5e8465fa6bd24b4c
kuntalkumar <kuntalkumar789@gmail.com>
2024-09-07 14:42:20 +0530

    handle some of the edge cases

--- chrome-extensions/fan/index.js
+++ chrome-extensions/fan/index.js
@@ -8,11 +8,14 @@ window.onload = function() {
     offbtn.style.display = "none";
 };
 
+
+
+
 function myFunction(speed) {
+    
     if (speed === 0) {
         image.style.animationDuration = '1s';
-        if( image1.style.animation == `fananim 0.2s linear infinite`){
-
+        if (image1.style.animation === 'fananim 0.2s linear infinite') {
             image1.style.animationDuration = '1s';
         }
         offbtn.style.display = "none";
@@ -20,11 +23,20 @@ function myFunction(speed) {
 
         setTimeout(() => {
             image.style.animationPlayState = 'paused';
-            if( image1.style.animation == `fananim 0.2s linear infinite`){
-
+            if (image1.style.animation === 'fananim 0.2s linear infinite') {
                 image1.style.animationPlayState = 'paused';
             }
         }, 2000);
+
+        if (turboCool.style.display === "none") {
+            image1.style.animationDuration = '1s';
+            image1.style.transition = 'animation-duration 3s ease';
+
+            setTimeout(() => {
+                image1.style.animationPlayState = 'paused';
+            }, 2000);
+            turboCool.style.display = "";
+        }
     } else if (speed === 0.2) {
         image.style.animation = `fananim ${speed}s linear infinite`;
         image1.style.animation = `fananim ${speed}s linear infinite`;
