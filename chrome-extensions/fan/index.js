7286dec3e37fe68a0f9b97f3e464575438b2e0fa
kuntalkumar <kuntalkumar789@gmail.com>
2024-09-07 14:18:22 +0530

    handle some of the edge cases

--- chrome-extensions/fan/index.js
+++ chrome-extensions/fan/index.js
@@ -4,59 +4,46 @@ let offbtn = document.getElementById("offbtn");
 let onbtn = document.getElementById("onbtn");
 let turboCool = document.getElementById("turboCool");
 
-// offbtn.style.display="none"
-window.onload(offbtn.style.display="none")
+window.onload = function() {
+    offbtn.style.display = "none";
+};
 
 function myFunction(speed) {
     if (speed === 0) {
-        image.style.transition = 'animation-duration 3s ease';
         image.style.animationDuration = '1s';
-        image1.style.transition = 'animation-duration 3s ease';
-        image1.style.animationDuration = '1s';
-        offbtn.style.display="none"
-        
-        onbtn.style.display=""
+        if( image1.style.animation == `fananim 0.2s linear infinite`){
+
+            image1.style.animationDuration = '1s';
+        }
+        offbtn.style.display = "none";
+        onbtn.style.display = "";
 
         setTimeout(() => {
             image.style.animationPlayState = 'paused';
-            image1.style.animationPlayState = 'paused';
+            if( image1.style.animation == `fananim 0.2s linear infinite`){
 
+                image1.style.animationPlayState = 'paused';
+            }
         }, 2000);
-    } 
-    else if(speed==0.2){
-        image.style.animation = `fananim ${speed}s linear infinite`; 
-        image1.style.animation = `fananim ${speed}s linear infinite`; 
-        onbtn.style.display="none"
-        offbtn.style.display=""
-        turboCool.style.display="none"
-
-    }
-    
-    
-    else {
+    } else if (speed === 0.2) {
         image.style.animation = `fananim ${speed}s linear infinite`;
+        image1.style.animation = `fananim ${speed}s linear infinite`;
+        onbtn.style.display = "none";
+        offbtn.style.display = "";
+        turboCool.style.display = "none";
+    } else {
+        image.style.animation = `fananim ${speed}s linear infinite`;
+        onbtn.style.display = "none";
+        offbtn.style.display = "";
 
-        onbtn.style.display="none"
-        offbtn.style.display=""
+        if (turboCool.style.display === "none") {
+            image1.style.animationDuration = '1s';
+            image1.style.transition = 'animation-duration 3s ease';
 
-
-if(turboCool.style.display=="none"){
-
-    image1.style.transition = 'animation-duration 3s ease';
-    image1.style.animationDuration = '1s';
-    image1.style.transition = 'animation-duration 3s ease';
-
-    
-    setTimeout(() => {
-        image1.style.animationPlayState = 'paused';
-        
-    }, 2000);
-    turboCool.style.display=""
-
-
-}
-
-
-     
+            setTimeout(() => {
+                image1.style.animationPlayState = 'paused';
+            }, 2000);
+            turboCool.style.display = "";
+        }
     }
 }
