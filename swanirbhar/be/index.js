6e840e7e91092982391324fd36a229ee927bd6f7
kuntalkumar <kuntalkumar789@gmail.com>
2024-09-10 15:25:19 +0530

    added

--- swanirbhar/be/index.js
+++ swanirbhar/be/index.js
@@ -8,6 +8,9 @@ const app=express();
 app.use(express.json())
 const mongoDbUrl=process.env.MONGODB_URL
 
+
+
+// code for user management 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -73,6 +76,76 @@ app.post("/login",async(req,res)=>{
 
 })
 
+//  below portion of code for task management
+
+const taskSchema = new mongoose.Schema({
+    task: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'progress', 'completed'],
+        default: 'pending'
+    },
+    priority : {
+                type:String
+    }
+}); 
+
+const TaskModel = mongoose.model("task", taskSchema);
+
+app.post("/addtask",async(req,res)=>{
+
+    const{task}=req.body
+    const newTask= await TaskModel.create({task})
+
+    res.status(201).json({message: newTask})
+
+
+})
+
+app.get("/task",async(req,res)=>{
+    const alltasks= await TaskModel.find()
+
+    res.status(201).json({message:alltasks})
+})
+
+app.put("/edit/:id", async(req,res)=>{
+
+    try {
+        const { status } = req.body;
+        const taskId = req.params.id;
+        const updatedTask = await TaskModel.findByIdAndUpdate(
+            taskId,
+            { status },
+            { new: true, runValidators: true } 
+        );
+        if (!updatedTask) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        res.status(200).json({ message: "Task edited successfully", updatedTask });
+}
+
+catch(err){
+            res.status(500).json({message : "server error while editing the tasks "})
+}
+})
+
+app.delete("/delete/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+        if (!deletedTask) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+        res.status(200).json({ message: "Task deleted successfully", deletedTask });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }
+});
 
 
 const port=8080
